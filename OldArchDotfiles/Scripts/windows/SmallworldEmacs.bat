c39bad421a05dbb6c739f736a9ccba785be4718c
vllblvck <m.komar12121998@gmail.com>
2021-08-05 19:22:05 +0200

    Moved used configs from OldArchDotfiles and removed useless ones

--- OldArchDotfiles/Scripts/windows/SmallworldEmacs.bat
+++
@@ -1,8 +0,0 @@
-REM Sets environment variable
-SET SMALLWORLD_GIS=C:\Smallworld43\product
-REM Runs emacs environment config script
-call C:\Smallworld43\product\config\environment.bat
-REM Sets starting directory in emacs
-cd C:\Users\Mateusz
-REM Runs emacs
-call C:\Smallworld43\emacs\bin\emacs.exe
