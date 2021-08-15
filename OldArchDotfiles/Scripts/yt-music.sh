cdc19ebe089c28fb62f3a1d858c390614194e676
vllblvck <m.komar12121998@gmail.com>
2021-08-15 21:43:44 +0200

    Renamed old configs directory

--- OldArchDotfiles/Scripts/yt-music.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-directory="$HOME/Music/%(title)s.%(ext)s"
-format="wav"
-quality=0
-url="https://www.youtube.com/playlist?list=PLZGNjNozIky0F5FyR_aVK4uChqYZr3oQD"
-
-youtube-dl -o $directory -x --audio-format $format --audio-quality $quality --prefer-ffmpeg $url 
