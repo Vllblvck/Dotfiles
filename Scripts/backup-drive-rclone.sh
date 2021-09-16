1ee5dd58563c1c7e79ac0e2c4bc492b6ae2abf96
vllblvck <m.komar12121998@gmail.com>
2021-09-16 19:13:51 +0200

    Added new script for google drive backup

---
+++ Scripts/backup-drive-rclone.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+REMOTE="google_drive"
+SOURCES=("$HOME/Documents/DigitalGarden/" "$HOME/Documents/Passwords.kdbx")
+DESTINATIONS=("DigitalGarden/" "Documents/")
+
+if [[ "${#SOURCES[@]}" -ne "${#DESTINATIONS[@]}" ]]
+then
+  echo "Number of SOURCES and DESTINATIONS is not equal"
+  exit 1
+fi
+
+for i in "${!SOURCES[@]}"
+do
+  rclone copy "${SOURCES[i]}" "$REMOTE":"${DESTINATIONS[i]}" -P
+done
